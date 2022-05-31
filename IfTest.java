2d5577112b13d67bec836bf3a34f3d11625b70ab
宮崎　雅愛 <miyazakimasae@miyazakitadashiainoMacBook-Air.local>
2022-05-31 21:10:45 +0900

    練習

---
+++ IfTest.java
@@ -0,0 +1,9 @@
+public class IfTest {
+    public static void main(String[] args){
+        int myAge = 35;
+        if(myAge == 35){
+            System.out.println("私の年齢は35歳です！");
+        }
+        System.out.println("私の出身地は・・・");
+    }
+}
