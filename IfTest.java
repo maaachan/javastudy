39f81bd3c07baf00c86a95c9516434e735b2ab15
宮崎　雅愛 <miyazakimasae@miyazakitadashiainoMacBook-Air.local>
2022-05-31 22:12:03 +0900

    hennkou

--- IfTest.java
+++ IfTest.java
@@ -1,7 +1,7 @@
 public class IfTest {
     public static void main(String[] args){
         int myAge = 35;
-        if(myAge == 35){
+        if(myAge == 34){
             System.out.println("私の年齢は34歳です！");
         }
         System.out.println("私の出身地は・・・");
