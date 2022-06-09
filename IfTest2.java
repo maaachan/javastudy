b7a003b744987e563e02cbfaa01d0b7e25f6d570
宮崎　雅愛 <miyazakimasae@miyazakitadashiainoMacBook-Air.local>
2022-06-09 16:00:17 +0900

    iftest2

---
+++ IfTest2.java
@@ -0,0 +1,16 @@
+public class IfTest2{
+public static void main(String[] args){
+    int myAge =40;
+    char myBloodType = 'A';
+    if(myAge == 35 && myBloodType == 'B'){
+        System.out.println("私の年齢は35歳で血液型は県です！");
+    } else if(myAge == 35 && myBloodType == 'A'){
+        System.out.println("私の年齢は35歳で血液型はA型です！");
+    } else if(myAge == 40 && myBloodType == 'A'){
+        System.out.println("私の年齢は40歳で血液型はA型です！");
+    }else{
+        System.out.println("私の年齢は35歳でないです！");
+    }
+    System.out.println("私の出身地は・・・");
+}
+}
