85bbfaf294db570a6a6eff9aea66c62a316bb635
宮崎　雅愛 <miyazakimasae@miyazakitadashiainoMacBook-Air.local>
2022-06-10 17:04:13 +0900

    while 例文2

---
+++ WhileTest2.java
@@ -0,0 +1,11 @@
+public class WhileTest {
+    public static void main(String[] args){
+        System.out.println("番号！");
+        int number = 1;                //番号(１から始まる）
+        while(number <= 10){           
+            System.out.println(number + "!!");
+            number++;
+        }
+    }
+    
+}
