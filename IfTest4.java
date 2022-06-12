fff84662afbf2228b8de415d7d7f8f6f431ddb8f
宮崎　雅愛 <m.3s.f.666hlv@gmail.com>
2022-06-12 09:19:04 +0900

    iiftest編集

--- IfTest4.java
+++ IfTest4.java
@@ -4,7 +4,6 @@ public class IfTest4{
         int b = 10;
         if (++a == 6 || --b ==10){  //前置と後置で比較する→単略評価は？
             System.out.println("条件成立！");
-            System.out.println("a" + a);
             System.out.println("b" + b);
         } else{
             System.out.println("条件不成立！");
