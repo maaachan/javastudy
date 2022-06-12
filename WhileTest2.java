4befa4be951459b312840b68599f42a9df8c1726
宮崎　雅愛 <miyazakimasae@miyazakitadashiainoMacBook-Air.local>
2022-06-12 09:07:15 +0900

    while変更

--- WhileTest2.java
+++ WhileTest2.java
@@ -1,4 +1,4 @@
-public class WhileTest {
+public class WhileTest2 {
     public static void main(String[] args){
         System.out.println("番号！");
         int number = 1;                //番号(１から始まる）
