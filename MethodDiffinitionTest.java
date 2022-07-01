2f99e3d7f311698f500c6d8b79707d6505ee18fb
宮崎　雅愛 <m.3s.f.666hlv@gmail.com>
2022-07-01 13:48:33 +0900

    メソッド呼び出し方2

--- MethodDiffinitionTest.java
+++ MethodDiffinitionTest.java
@@ -1,24 +1,23 @@
 public class MethodDiffinitionTest {
     public static void main(String[] args){   //エントリーポイント（プログラミングの入口）
        System .out .println ("mainメソッド開始");
-       add();
-       add2();
+       int number1 = 5;
+       int number2 = 10;
+       int sum = 0;
+       // 足し算する処理
+       add(number1,number2);
+       // 足し算した結果を画面に表示する処理
+       printSum(sum);
        System.out.println("mainメソッド終了");
     }
 
-    public static void add(){
+    public static void add(int number1, int number2){
         System.out.println("add1メソッド開始");
-        int number1 = 5;
-        int number2 = 10;
-        int sum = 0;
-        sum = number1 + number2;
-
         System.out.println("number1:" + number1);
         System.out.println("number2:" + number2);
-        System.out.println("sum:" + sum);
-        
+        int sum = 0;
+        sum = number1 + number2;
         System.out.println("add1メソッド終了");
-
     }
     public static void add2(){
         System.out.println("add2メソッド開始");
@@ -30,6 +29,13 @@ public class MethodDiffinitionTest {
         System.out.println("number2:" + number2);
         System.out.println("sum:" + sum);
         System.out.println("add2メソッド終了");
+    }   // メソッドのとじカッコ：呼び出したところに戻る
+    public static void printSum(int sum){
+        System.out.println("printsumメソッド開始");
+        System.out.println("sum:" + sum);
+        System.out.println("printsumメソッド終了");
+
+
     }
     
 }
